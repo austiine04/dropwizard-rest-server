c588b2a6353f8587caac99134c7df3469716c139
Kisitu Augustine <austiine04@gmail.com>
2014-06-16 12:21:49 +0300

    displaying menu items json

--- src/main/resources/assets/js/app.js
+++ src/main/resources/assets/js/app.js
@@ -1,5 +1,18 @@
 var app = angular.module('restaurant', []);
 
-app.controller('MenuItemsController', function($scope){
-    $scope.name = "Hello World";
+app.service('MenuItemsService', function ($http) {
+
+    return {
+        fetchMenuItems:  function () {
+            return $http.get('/api/menu_items');
+        }
+    }
+});
+
+app.controller('MenuItemsController', function ($scope, MenuItemsService) {
+
+    MenuItemsService.fetchMenuItems().then(function (response) {
+        $scope.menuItems = response.data;
+    });
+
 });
