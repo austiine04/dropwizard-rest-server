95656fa00e5cd3d01848a6633f712aa478cf2952
Kisitu Augustine <austiine04@gmail.com>
2014-06-17 15:48:44 +0300

    submit new menu items to the server

--- src/main/resources/assets/js/app.js
+++ src/main/resources/assets/js/app.js
@@ -3,16 +3,25 @@ var app = angular.module('restaurant', []);
 app.service('MenuItemsService', function ($http) {
 
     return {
-        fetchMenuItems:  function () {
+        get: function () {
             return $http.get('/api/menu_items');
+        },
+
+        create: function (menuItem) {
+            return $http.post('/api/menu_items', menuItem);
         }
     }
 });
 
 app.controller('MenuItemsController', function ($scope, MenuItemsService) {
 
-    MenuItemsService.fetchMenuItems().then(function (response) {
+    MenuItemsService.get().then(function (response) {
         $scope.menuItems = response.data;
     });
+
+    $scope.createItem = function () {
+        $scope.menuItems.push($scope.item);
+        MenuItemsService.create($scope.item);
+    };
 
 });
